905f037b9a5c724540eeebc58802d88ebb55c8ca
Marius Stein <mariusstein7@gmail.com>
2025-01-22 14:11:21 +0100

    another test

--- src/main/java/solutions/stein/demo/controller/AppStartStatResponse.java
+++ src/main/java/solutions/stein/demo/controller/AppStartStatResponse.java
@@ -1,17 +1,44 @@
 package solutions.stein.demo.controller;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class AppStartStatResponse {
 
     private long startTimestamp;
     private long initializedTimestamp;
     private long requestReceivedTimestamp;
-
+    
     private String podName;
     private String podNamespace;
+    
+    public long getStartTimestamp() {
+        return startTimestamp;
+    }
+    public void setStartTimestamp(long startTimestamp) {
+        this.startTimestamp = startTimestamp;
+    }
+    public long getInitializedTimestamp() {
+        return initializedTimestamp;
+    }
+    public void setInitializedTimestamp(long initializedTimestamp) {
+        this.initializedTimestamp = initializedTimestamp;
+    }
+
+    public long getRequestReceivedTimestamp() {
+        return requestReceivedTimestamp;
+    }
+    public void setRequestReceivedTimestamp(long requestReceivedTimestamp) {
+        this.requestReceivedTimestamp = requestReceivedTimestamp;
+    }
+    public String getPodName() {
+        return podName;
+    }
+    public void setPodName(String podName) {
+        this.podName = podName;
+    }
+    public String getPodNamespace() {
+        return podNamespace;
+    }
+    public void setPodNamespace(String podNamespace) {
+        this.podNamespace = podNamespace;
+    }
     
 }
